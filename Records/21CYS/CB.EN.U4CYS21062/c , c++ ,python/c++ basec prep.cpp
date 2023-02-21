0878b43f980e941b30fd0ce45578ded39cd1d59b
Ruthwik2610 <86421510+Ruthwik2610@users.noreply.github.com>
2023-02-21 22:27:10 +0530

    Add files via upload
    added more python and c c++ files

---
+++ Records/21CYS/CB.EN.U4CYS21062/c , c++ ,python/c++ basec prep.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Employee{
+public:
+string Name;
+string Company;
+int Age;
+void Introduce_yourself(){
+    
+}
+};
+int main()
+{
+Employee employee1;
+employee1.Age;
+cout<<"hii";
+return 0;
+}
